da56869b33e3760cec784f8411fdd9fdb1dcce8f
Santiago González T <santiago.dgt9@gmail.com>
2023-11-28 10:24:35 -0600

    Modificacion_ordenAtentido
    Agregue algunos metodos del CRUD

--- src/atencion/OrdenAtencion.java
+++ src/atencion/OrdenAtencion.java
@@ -1,4 +1,3 @@
-
 package atencion;
 
 /**
@@ -7,31 +6,42 @@ package atencion;
  */
 import java.util.ArrayList;
 
+public class OrdenAtencion extends Paciente {
 
-public class OrdenAtencion extends Paciente{
+    private Paciente paciente;
 
-    ArrayList <String> pacientes ;
+    ArrayList<String> pacientes;
 
     public OrdenAtencion(ArrayList<String> pacientes) {
-        this.pacientes = paciente;
-    }
-    
-    pacientes.add = Paciente
-   
-   
-    
-            
-    public boolean atendito(String HoraSalida){
-    if(HoraSalida Equals.getHoraSalida !Empity("")){
-        
-     return true;
-    }
-       return false;
-    
+        this.pacientes = Paciente;
     }
 
-  
-    
-  
+    public void Añadir() {
+        pacientes.add = paciente
     }
-    
+
+    public boolean Borrar(String Cedula) {
+        if (Cedula = Equals(getCedula)   {
+            pacientes.remove(this.paciente);
+            return true;
+        }
+        return false;
+
+    }
+
+    public void Actualizar() {
+        pacientes.replace()
+    }
+
+    public void MoverPaciente() {
+        pacientes.set(0, paciente)
+    }
+
+    public boolean atendito(String HoraSalida) {
+        if (HoraSalida.equals(paciente.getHoraSalida().isEmpty(""))) {
+            return true;
+        }
+        return false;
+
+    }
+}
